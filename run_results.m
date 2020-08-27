02b61220011b25fa620ff6c7a6aac2e7e481cc80
carisio <carisio@gmail.com>
2020-08-27 12:00:53 -0300

    Calculate the cummulative ER/TER for distances < some value

--- run_results.m
+++ run_results.m
@@ -52,3 +52,34 @@ end
 fprintf('\nConsidering all base stations\n');
 fprintf('S\t\t\tTER\n');
 fprintf('%2f\t%2f\n', sum(S), TER);
+
+% Let's plot the accumulated TER considering the influence ring a ring
+% we are considering that the inner ring is the same regardless the
+% frequency
+% PS.: The calculation below assumes that the inner ring is the same
+% for all frequencies. 
+nRings = 82;
+TER_acc = zeros(nRings, 1);
+nFreq = length(freq);
+S_acc = zeros(nFreq, nRings);
+ER_acc = zeros(nFreq, nRings);
+for f=1:nFreq
+    % We are using two propagation models. It is necessary two calculate
+    % the contribution of both and select the one that apply
+    S_pl_model_1 = eq4_finite_rings(C(f,1), gamma(1), R(f), EIRP(f), freq(f), 1:nRings);
+    S_pl_model_2 = eq4_finite_rings(C(f,2), gamma(2), R(f), EIRP(f), freq(f), 1:nRings);
+    use_PL_1 = (1:nRings)* R(f) < d_transition(f);
+    S_at_ring = (use_PL_1 .* S_pl_model_1) + (1 - use_PL_1).*S_pl_model_2;
+    for r=1:nRings
+        S_acc(f,r) = sum(S_at_ring(1:r));
+    end
+    ER_acc(f, :) = 100 * S_acc(f, :) / S_lim(f);
+end
+TER_acc = sum(ER_acc, 1);
+TER_lim = ones(nRings)*TER;
+percentage_acc_to_lim = TER_acc/TER;
+d = R(1)*(1:nRings);
+figure(1);
+plot(d, TER_acc, d, TER_lim);
+figure(2);
+plot(d, percentage_acc_to_lim);
